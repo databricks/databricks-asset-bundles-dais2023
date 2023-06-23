6cf4721b674f98248b27ce7d77214d89ac5bae89
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-06-23 16:04:04 +0200

    Add GHA workflows

---
+++ .github/support/dlt/__init__.py
@@ -0,0 +1,121 @@
+import functools
+import inspect
+import pathlib
+from pprint import pprint
+from typing import Dict
+
+
+_flows = {}
+
+_repository_root = pathlib.Path(__file__).parent.parent.parent.parent
+
+
+class Flow():
+    expectations: Dict[str, 'Expectation']
+
+    def __init__(self, func):
+        self.func = func
+        self.name = func.__name__
+        self.expectations = {}
+
+        file = inspect.getsourcefile(func)
+        (lines, lineno) = inspect.getsourcelines(func)
+
+        # Find line where function is defined
+        actuallineno = lineno
+        for line in lines:
+            if line.strip().startswith('def ' + self.name):
+                break
+            actuallineno += 1
+
+        self.file = file
+        self.relpath = pathlib.Path(self.file).relative_to(_repository_root)
+        self.lineno = actuallineno
+
+
+    def __repr__(self):
+        return f'Flow({self.name} ({self.file}:{self.lineno})))'
+
+
+class Expectation():
+    def __init__(self, name, expr, typ):
+        self.name = name
+        self.expr = expr
+        self.typ = typ
+
+
+def _get_or_create_flow(func) -> Flow:
+    flow = Flow(func)
+    if flow.name not in _flows:
+        _flows[flow.name] = flow
+    return _flows[flow.name]
+
+
+def table(_func = None, *, name=None, expr=None):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        return wrapper
+
+    if _func is None:
+        return decorator
+    else:
+        return decorator(_func)
+
+
+def view(_func = None, *, name=None, expr=None):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        return wrapper
+
+    if _func is None:
+        return decorator
+    else:
+        return decorator(_func)
+
+
+def expect_or_drop(name, expr):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        exp = Expectation(name, expr, typ='expect_or_drop')
+        _get_or_create_flow(func).expectations[exp.name] = exp
+        return wrapper
+    return decorator
+
+
+def expect_or_fail(name, expr):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        exp = Expectation(name, expr, typ='expect_or_fail')
+        _get_or_create_flow(func).expectations[exp.name] = exp
+        return wrapper
+    return decorator
+
+
+def expect(name, expr):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        exp = Expectation(name, expr, typ='expect')
+        _get_or_create_flow(func).expectations[exp.name] = exp
+        return wrapper
+    return decorator
+
+
+def print_source_map():
+    pprint(_flows)
+
+
+def get_flow(name) -> Flow:
+    return _flows[name]
---
+++ .github/support/emit_annotations.py
@@ -0,0 +1,53 @@
+import sys
+
+import dlt
+from databricks.sdk import WorkspaceClient
+
+def main():
+    import count_claps
+    import input
+
+    pipeline_id = sys.argv[1]
+    update_id = sys.argv[2]
+
+    w = WorkspaceClient(host="https://e2-demo-west.cloud.databricks.com/")
+    events = w.pipelines.list_pipeline_events(
+        pipeline_id=pipeline_id,
+    )
+
+    for event in events:
+        # Focus on most recent update.
+        if event.origin.update_id != update_id:
+            break
+
+        if event.event_type != 'flow_progress':
+            continue
+
+        flow_name = event.origin.flow_name
+        flow = dlt.get_flow(flow_name)
+
+        flow_progress = event.details['flow_progress']
+        flow_status = flow_progress['status']
+
+        if flow_status == 'COMPLETED':
+            data_quality = flow_progress['data_quality']
+
+            for exp in data_quality['expectations']:
+                passed_records = exp['passed_records']
+                failed_records = exp['failed_records']
+                total_records = passed_records + failed_records
+                name = exp['name']
+                if failed_records == 0:
+                    message = f"Expectation '{name}' passed for {passed_records} records"
+                    print(f"::notice file={flow.relpath},line={flow.lineno}::{message}")
+                else:
+                    message = f"Expectation '{name}' failed for {failed_records}/{total_records} records"
+                    print(f"::warning file={flow.relpath},line={flow.lineno}::{message}")
+
+        if flow_status == 'FAILED':
+            message = f"Expectation '{name}' failed"
+            print(f"::error file={flow.relpath},line={flow.lineno}::{message}")
+
+
+if __name__ == '__main__':
+    main()
